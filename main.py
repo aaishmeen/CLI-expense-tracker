2a94614cf31f5f132d863444ef8e1b61344e4b2b
aaishmeen <chowdharyaaishmeen@gmail.com>
2026-05-31 15:52:02 +0530

    feat: add expense deletion and enhance CLI usuability

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-expenses =[]
+import os
+
+def clear_screen():
+    os.system("cls")
+
+def pause():
+    input('\nPress Enter to continue....')
+    clear_screen() 
+
+expenses = []
 
 
 while True:
@@ -12,7 +21,7 @@ while True:
     print("4. Delete Expense")
     print("5. EXIT")
 
-    choice = input("Enter your choice (1-3): ")
+    choice = input("Enter your choice (1-5): ")
 
     match choice:
         case "1":
@@ -29,34 +38,72 @@ while True:
             expenses.append(expense)
 
             print("Expense added successfully")
+            pause()
 
 
         case "2":
-            print('-' *50)
-            print(f"{'Category':<15} {'Item':<20} {'Price':<10}")
-            print("-" * 50)
+            if not expenses :
+                print("No Expenses Found")
+                pause() 
+            
+            else:
+    
+                print('-' *50)
+                print(f"{'Category':<15} {'Item':<20} {'Price':<10}")
+                print("-" * 50)
 
-            for expense in expenses:
-                print(f"{expense['category']:<15} {expense['item_name']:<20} ₹{expense['price']:<10}")
-               
-            print("-" * 50)  
+                for expense in expenses:
+                    print(f"{expense['category']:<15} {expense['item_name']:<20} ₹{expense['price']:<10}")
+                
+                print("-" * 50)
+                pause() 
 
          
         case "3":
             total =0
-            if len(expenses)==0:
+            if not expenses :
                 print("No Expenses Found")
-           
-            for expense in expenses :
-                total+= expense["price"]
+                pause()
 
-            print("-" * 50 )    
-            print(f"Total Spending : ₹{total}")    
-            print("-" * 50 )    
+            else:
+                for expense in expenses :
+                    total+= expense["price"]
+
+                print("-" * 50 )    
+                print(f"Total Spending : ₹{total}")    
+                print("-" * 50 )
+                
+                pause()    
        
         case "4":
-            pass
+            if not expenses :
+              print("No Expenses Found")
+              pause()
 
+            else:  
+
+                print("-" * 65)
+                print(f"{'No.':<5} {'Category':<15} {'Item':<20} {'Price':<10}")
+                print("-" * 65)
+
+                for index, expense in enumerate(expenses, start=1):
+                    print(
+                        f"{index:<5} "
+                        f"{expense['category']:<15} "
+                        f"{expense['item_name']:<20} "
+                        f"₹{expense['price']:<10}"
+                    )
+                print("-" * 65)  
+           
+                delete_exp = int(input("\nEnter expense to be deleted : "))
+
+                if 1<=delete_exp <= len(expenses):
+                    deleted_expense= expenses.pop(delete_exp-1)
+                    print(f"{deleted_expense['item_name']} Deleted Successfully")
+                    pause()
+                else :
+                    print("Invalid expense number")
+                    pause()
 
         case "5":
             print("EXITING NOW !")
@@ -64,6 +111,7 @@ while True:
         
         case _:
             print("Invalid Choice")
+            pause()
 
 
 
